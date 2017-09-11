fa0afa395eab36bde50ef9099fb9e111e69e9a84
mr0p0hurd <hard-k5@yandex.ru>
2017-09-11 18:55:05 +0300

    HJ_4

---
+++ src/SeaBattle.java
@@ -0,0 +1,12 @@
+public class SeaBattle {
+
+    public static void main(String[] args) {
+        String[][] numb = {
+                {"*", "0", "0", "0", "0"},
+                {"*", "0", "0", "0", "0"},
+                {"*", "0", "0", "0", "0"},
+                {"*", "0", "0", "0", "0"}
+        };
+    }
+}
+
